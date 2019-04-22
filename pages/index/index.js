f37f9f1bf442c1ad43b22766d10c7af31519b242
qinxuan <yiqinxuan>
2019-04-22 09:56:55 +0800

    删除index，修改tab为index，index为默认加载页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,65 +1,169 @@
-//index.js
-//获取应用实例
-const app = getApp()
+//appo.js
+const util = require('../../utils/util.js')
+
+
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+      currentTab : 0,
+      addressTab : 0,
+      addressName1: "龙江店",
+      addressName2: "龙山店",
+      addressName3: "龙店",
+      noOrder: true,
+      dateSelect:'',
+      addressSelect:'',
+      btnColor:false,
+      orderDate:'',
+      orderAddress:'',
+      orderTel:'',
+      orderLists:'',
+      appointmentId:'',
+      appointmentId1:'',
+
   },
   onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+
+      var timeT = util.formatDate(new Date());
+      var dateT = util.getDates(3, timeT);
+      // console.log(dateT);
+      if(!this.data.havePlace1){
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+              timeAm: '已满'
           })
-        }
+      }
+      if(!this.data.havePlace2){
+          this.setData({
+              timePm: '已满'
+          })
+      }
+
+      this.setData({
+          dateNum1: dateT[0].time,
+          weekNum1: dateT[0].week,
+          dateNum2: dateT[1].time,
+          weekNum2: dateT[1].week,
+          dateNum3: dateT[2].time,
+          weekNum3: dateT[2].week
+
       })
-    }
+      this.queryInfo(0,0);
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-    navigateTo: function () {
-        wx.navigateTo({
-            url: '../tabs/tabs',
-            complete: function() {
-                console.log('打开页面后当前页面栈', getCurrentPages());
+
+    //点击切换选择日期
+    clickTab: function (e) {
+        const that = this;
+        if (that.data.currentTab === e.currentTarget.dataset.current) {
+            return false;
+        } else {
+          // console.log(e.currentTarget.dataset.current);
+            that.setData({
+                currentTab: e.currentTarget.dataset.current,
+            });
+        }
+        console.log(that.data.currentTab);
+        this.queryInfo(that.data.addressTab,that.data.currentTab);
+    },
+    //点击切换选择地点
+    addressTab: function (e) {
+        const that = this;
+        if (this.data.addressTab === e.currentTarget.dataset.address) {
+            return false;
+        } else {
+            // console.log(e.currentTarget.dataset.address);
+            that.setData({
+                addressTab: e.currentTarget.dataset.address,
+            })
+        }
+        console.log(this.data.addressTab);
+        this.queryInfo(this.data.addressTab,this.data.currentTab);
+    },
+    queryInfo:function(shopId,workTime){
+      const that = this;
+      if (shopId==0){
+          shopId = 'ls'
+      }else if(shopId==1){
+          shopId = 'lj'
+      }else if(shopId == 2){
+          shopId = 'rg'
+      }
+      /*if(workTime==0){
+          workTime = ''
+      }*/
+        wx.request({
+            url: 'https://qinxuan.club/dog-mini/customer/appointmentPage.do',
+            data: {
+                shopId: shopId,
+                workTime: '2019/04/21',
+                openid: wx.getStorageSync('openid')
+            },
+            success(res) {
+                console.log(res);
+                //const list = res.data.CustomerAppointment.petLists;
+                //console.log(list);
+
+                 if(res.data.sysStatue==1){
+                     that.setData({
+                         btnColor: true
+                     });
+                 }else{
+                     that.setData({
+                         btnColor: false
+                     });
+                 }
+                if(res.data.isApp==1){
+                    that.setData({
+                        noOrder: false,
+                        orderDate: workTime,
+                        orderAddress:shopId,
+                        orderTel:res.data.CustomerAppointment.phone,
+                        appointmentId:res.data.CustomerAppointment.appointmentId,
+
+                        // orderLists:res.data.CustomerAppointment.petLists,
+
+                    });
+                }else{
+                    that.setData({
+                        noOrder: true
+                    });
+                }
             }
         });
-        setTimeout(() => {
-            console.log('打开页面后当前页面栈123', getCurrentPages());
-        }, 1000);
-    }
-})
+    },
+
+    goappo:function(){
+        wx.navigateTo({
+            url: '../appo/appo'
+        })
+    },
+    cancelOrder:function(){
+        const that = this;
+        wx.request({
+            url: 'https://qinxuan.club/dog-mini/customer/cancelApponitment.do',
+            data: {
+                appointmentId: that.data.appointmentId,
+            },
+            success(res) {
+                console.log(res);
+            }
+        });
+
+    },
+    //上午的预约时间
+    bindTimeChangeAm: function (e) {
+        //console.log('picker发送选择改变，携带值为', e.detail.value);
+        this.setData({
+            timeAm: e.detail.value
+        })
+    },
+    //下午的预约时间
+    bindTimeChangePm: function (e) {
+        //console.log('picker发送选择改变，携带值为', e.detail.value);
+        this.setData({
+            timePm: e.detail.value
+        })
+    },
+
+
+});
+
